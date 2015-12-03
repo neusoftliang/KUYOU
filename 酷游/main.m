2ed70cc9019363f3bf59b1786893b37cddb0d44b
tarena <tarena@tarenadeimac-16.local>
2015-12-03 19:52:23 +0800

    Initial Commit

---
+++ 酷游/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  酷游
+//
+//  Created by tarena on 15/12/3.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
